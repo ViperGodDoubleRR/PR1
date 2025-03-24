16eeb5cadcd75b9f15489b81992821915fb51418
Арсений Куприянович <Arsenykupryianovich@yandex.by>
2025-03-24 09:30:41 +0300

    Создан класс

--- PR1/Program.cs
+++ PR1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace GIT
+{
+    class Dog
+    {
+        public string Nickname { get; set; }
+        public string Breed { get; set; }
+        public float Age { get; set; }
+
+        public Dog(string nickname, string breed, float age)
+        {
+            Nickname = nickname;
+            Breed = breed;
+            Age = age;
+        }
+
+        public string GetInfo()
+        {
+            return $"Собака: {Nickname}, Порода: {Breed}, Возраст: {Age} лет";
+        }
+
+        public bool IsOlderThan(float age)
+        {
+            return Age > age;
+        }
+    }
+
+}
